172b50ae0b3f4b6c512bc208ca4193b0341f17c8
L968 <lucas.c968@gmail.com>
2023-06-05 02:09:37 -0300

    Fixing inverted automapper profile

--- backend/Ecommerce.Application/Mappings/DomainToDTOMappingProfile.cs
+++ backend/Ecommerce.Application/Mappings/DomainToDTOMappingProfile.cs
@@ -1,13 +1,11 @@
-﻿using AutoMapper;
-
-namespace Ecommerce.Application.Mappings;
+﻿namespace Ecommerce.Application.Mappings;
 
 public class DomainToDTOMappingProfile : Profile
 {
 	public DomainToDTOMappingProfile()
 	{
         CreateMap<Address, GetAddressDto>().ReverseMap();
-        CreateMap<Address, CreateAddressDto>();
-        CreateMap<Address, UpdateAddressDto>();
+        CreateMap<CreateAddressDto, Address>();
+        CreateMap<UpdateAddressDto, Address>();
     }
 }
