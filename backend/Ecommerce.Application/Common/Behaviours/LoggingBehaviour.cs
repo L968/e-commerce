5e1d4323d1c5f6cdaa0eb1bae32f3280966e3fca
L968 <lucas.c968@gmail.com>
2023-06-11 21:12:30 -0300

    Adding logging behaviour to the mediator

---
+++ backend/Ecommerce.Application/Common/Behaviours/LoggingBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using MediatR.Pipeline;
+using Microsoft.Extensions.Logging;
+
+namespace Ecommerce.Application.Common.Behaviours;
+
+public class LoggingBehaviour<TRequest> : IRequestPreProcessor<TRequest> where TRequest : notnull
+{
+    private readonly ILogger _logger;
+    private readonly ICurrentUserService _currentUserService;
+
+    public LoggingBehaviour(ILogger<TRequest> logger, ICurrentUserService currentUserService)
+    {
+        _logger = logger;
+        _currentUserService = currentUserService;
+    }
+
+    public Task Process(TRequest request, CancellationToken cancellationToken)
+    {
+        string requestName = typeof(TRequest).Name;
+        int? userId = _currentUserService.UserId;
+
+        _logger.LogInformation("CleanArchitecture Request: {Name} {@UserId} {@Request}",
+            requestName, userId, request);
+
+        return Task.CompletedTask;
+    }
+}
