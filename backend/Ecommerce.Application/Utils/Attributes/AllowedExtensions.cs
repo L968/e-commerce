018ac654ca4720fcf25ef968d966cff11a6a2b05
L968 <lucas.c968@gmail.com>
2023-06-21 21:11:40 -0300

    Fixing upload attributes to accept the IFormFile type

--- backend/Ecommerce.Application/Utils/Attributes/AllowedExtensions.cs
+++ backend/Ecommerce.Application/Utils/Attributes/AllowedExtensions.cs
@@ -13,13 +13,11 @@ public class AllowedExtensions : ValidationAttribute
 
     protected override ValidationResult IsValid(object value, ValidationContext validationContext)
     {
-        if (value == null) return new ValidationResult("Value not provided");
+        if (value is null) return new ValidationResult("Value not provided");
 
-        var files = value as IFormFileCollection;
-
-        foreach (IFormFile file in files)
+        if (value is IFormFile file)
         {
-            if (file != null)
+            if (file is not null)
             {
                 var extension = Path.GetExtension(file.FileName);
                 if (!_extensions.Contains(extension.ToLower()))
@@ -27,6 +25,24 @@ public class AllowedExtensions : ValidationAttribute
                     return new ValidationResult(GetErrorMessage());
                 }
             }
+        }
+        else if (value is IFormFileCollection fileCollection)
+        {
+            foreach (IFormFile fileItem in fileCollection)
+            {
+                if (fileItem is not null)
+                {
+                    var extension = Path.GetExtension(fileItem.FileName);
+                    if (!_extensions.Contains(extension.ToLower()))
+                    {
+                        return new ValidationResult(GetErrorMessage());
+                    }
+                }
+            }
+        }
+        else
+        {
+            return new ValidationResult("Invalid value type");
         }
 
         return ValidationResult.Success!;
--- backend/Ecommerce.Application/Utils/Attributes/MaxFileSize.cs
+++ backend/Ecommerce.Application/Utils/Attributes/MaxFileSize.cs
@@ -11,21 +11,36 @@ public class MaxFileSize : ValidationAttribute
         _maxFileSize = maxFileSize;
     }
 
-    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
     {
-        if (value == null) return new ValidationResult("Value not provided");
+        if (value is null) return new ValidationResult("Value not provided");
 
-        var files = value as IFormFileCollection;
-
-        foreach (IFormFile file in files)
+        if (value is IFormFile file)
         {
-            if (file != null)
+            if (file is not null)
             {
                 if (file.Length > _maxFileSize)
                 {
                     return new ValidationResult(GetErrorMessage());
                 }
             }
+        }
+        else if (value is IFormFileCollection fileCollection)
+        {
+            foreach (IFormFile fileItem in fileCollection)
+            {
+                if (fileItem is not null)
+                {
+                    if (fileItem.Length > _maxFileSize)
+                    {
+                        return new ValidationResult(GetErrorMessage());
+                    }
+                }
+            }
+        }
+        else
+        {
+            return new ValidationResult("Invalid value type");
         }
 
         return ValidationResult.Success!;
