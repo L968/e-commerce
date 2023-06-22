fe48e5e137b9e00452ddb79b59a0090409a1be71
L968 <lucas.c968@gmail.com>
2023-06-21 21:13:23 -0300

    Removing unnecessary DTO folder

--- backend/Ecommerce.Application/DTO/ProductInventoryDto/UpdateProductInventoryDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Ecommerce.Application.DTO.ProductInventoryDto;
-
-public record UpdateProductInventoryDto
-{
-    [Required]
-    [Range(0, int.MaxValue, ErrorMessage = "Only positive numbers allowed")]
-    public int? Quantity { get; init; }
-}
