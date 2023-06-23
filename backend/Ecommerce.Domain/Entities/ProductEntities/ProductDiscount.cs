3d797f4866becc25c3a3164ed2b99db8c9ea0db9
L968 <lucas.c968@gmail.com>
2023-06-22 21:35:29 -0300

    Updating product discount entity to rich domain model

--- backend/Ecommerce.Domain/Entities/ProductEntities/ProductDiscount.cs
+++ backend/Ecommerce.Domain/Entities/ProductEntities/ProductDiscount.cs
@@ -3,13 +3,55 @@
 public sealed class ProductDiscount : AuditableEntity
 {
     public int Id { get; private set; }
+    public Guid ProductId { get; private init; }
     public string Name { get; private set; }
     public decimal DiscountValue { get; private set; }
     public string DiscountUnit { get; private set; }
     public decimal? MaximumDiscountAmount { get; private set; }
     public DateTime ValidFrom { get; private set; }
     public DateTime? ValidUntil { get; private set; }
-    public int ProductId { get; private set; }
 
     public Product? Product { get; set; }
+
+    public ProductDiscount(
+        Guid productId,
+        string name,
+        decimal discountValue,
+        string discountUnit,
+        decimal? maximumDiscountAmount,
+        DateTime validFrom,
+        DateTime? validUntil
+    )
+    {
+        ProductId = productId;
+        Name = name;
+        DiscountValue = discountValue;
+        DiscountUnit = discountUnit;
+        MaximumDiscountAmount = maximumDiscountAmount;
+        ValidFrom = validFrom;
+        ValidUntil = validUntil;
+    }
+
+    public void Update(
+        string name,
+        decimal discountValue,
+        string discountUnit,
+        decimal? maximumDiscountAmount,
+        DateTime validFrom,
+        DateTime? validUntil
+    )
+    {
+        Name = name;
+        DiscountValue = discountValue;
+        DiscountUnit = discountUnit;
+        MaximumDiscountAmount = maximumDiscountAmount;
+        ValidFrom = validFrom;
+        ValidUntil = validUntil;
+    }
+
+    public bool IsCurrentlyValid()
+    {
+        var currentDate = DateTime.UtcNow;
+        return ValidFrom <= currentDate && (ValidUntil == null || ValidUntil >= currentDate);
+    }
 }
