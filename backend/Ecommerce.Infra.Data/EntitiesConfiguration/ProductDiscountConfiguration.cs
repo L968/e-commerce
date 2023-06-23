1127186c08bb16f9e622c14e34c91a04f54c4400
L968 <lucas.c968@gmail.com>
2023-06-22 21:35:06 -0300

    Adding product discount relation to context

---
+++ backend/Ecommerce.Infra.Data/EntitiesConfiguration/ProductDiscountConfiguration.cs
@@ -0,0 +1,12 @@
+﻿namespace Ecommerce.Infra.Data.EntitiesConfiguration;
+
+public class ProductDiscountConfiguration : IEntityTypeConfiguration<ProductDiscount>
+{
+    public void Configure(EntityTypeBuilder<ProductDiscount> builder)
+    {
+        builder
+            .HasOne(productDiscount => productDiscount.Product)
+            .WithMany(product => product.Discounts)
+            .HasForeignKey(productDiscount => productDiscount.ProductId);
+    }
+}
