82e0380e66b3efbd6eb395ff18cc0d1f63e4fa94
L968 <lucas.c968@gmail.com>
2023-06-25 00:23:24 -0300

    Adding product discount queries

---
+++ backend/Ecommerce.Application/ProductDiscounts/Queries/GetProductDiscountDto.cs
@@ -0,0 +1,15 @@
+﻿using Ecommerce.Domain.Enums;
+
+namespace Ecommerce.Application.ProductDiscounts.Queries;
+
+public class GetProductDiscountDto
+{
+    public int Id { get; set; }
+    public Guid ProductId { get; set; }
+    public string Name { get; set; } = "";
+    public decimal DiscountValue { get; set; }
+    public DiscountUnit DiscountUnit { get; set; }
+    public decimal? MaximumDiscountAmount { get; set; }
+    public DateTime ValidFrom { get; set; }
+    public DateTime? ValidUntil { get; set; }
+}
