80271df1da6cfc890f84cad3f56ed9e71bbd4e98
L968 <lucas.c968@gmail.com>
2023-06-25 00:24:27 -0300

    Adding product discount controller

---
+++ backend/Ecommerce.API/Controllers/ProductDiscountController.cs
@@ -0,0 +1,34 @@
+﻿using Ecommerce.Application.ProductDiscounts.Commands.CreateProductDiscount;
+using Ecommerce.Application.ProductDiscounts.Queries;
+
+namespace Ecommerce.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize(Roles = "admin")]
+    public class ProductDiscountController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ProductDiscountController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _mediator.Send(new GetProductDiscountsQuery()));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateProductDiscountCommand command)
+        {
+            Result<GetProductDiscountDto> result = await _mediator.Send(command);
+            if (result.IsFailed) return BadRequest(result.Reasons);
+
+            var productDiscount = result.Value;
+            return CreatedAtAction(nameof(Get), null, productDiscount);
+        }
+    }
+}
