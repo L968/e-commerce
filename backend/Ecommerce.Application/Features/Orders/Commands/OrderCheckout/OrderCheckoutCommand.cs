0fe7cfc9bf30eb4566232318cc4db717e7dd329c
L968 <lucas.c968@gmail.com>
2023-07-09 17:15:25 -0300

    Adding the OrderCheckout command and it's validator

---
+++ backend/Ecommerce.Application/Features/Orders/Commands/OrderCheckout/OrderCheckoutCommand.cs
@@ -0,0 +1,53 @@
+﻿namespace Ecommerce.Application.Features.Orders.Commands.OrderCheckout;
+
+[Authorize]
+public record OrderCheckoutCommand : IRequest<Result>
+{
+    public int UserId { get; set; }
+    public IEnumerable<CartItem> CartItems { get; set; }
+    public string ShippingPostalCode { get; set; } = "";
+    public string ShippingStreetName { get; set; } = "";
+    public string ShippingBuildingNumber { get; set; } = "";
+    public string? ShippingComplement { get; set; }
+    public string? ShippingNeighborhood { get; set; }
+    public string? ShippingCity { get; set; }
+    public string? ShippingState { get; set; }
+    public string? ShippingCountry { get; set; }
+}
+
+public class OrderCheckoutCommandHandler : IRequestHandler<OrderCheckoutCommand, Result>
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IOrderRepository _orderRepository;
+
+    public OrderCheckoutCommandHandler(IUnitOfWork unitOfWork, IOrderRepository orderRepository)
+    {
+        _unitOfWork = unitOfWork;
+        _orderRepository = orderRepository;
+    }
+
+    public async Task<Result> Handle(OrderCheckoutCommand request, CancellationToken cancellationToken)
+    {
+        var result = Order.Create(
+            request.UserId,
+            request.CartItems,
+            request.ShippingPostalCode,
+            request.ShippingStreetName,
+            request.ShippingBuildingNumber,
+            request.ShippingComplement,
+            request.ShippingNeighborhood,
+            request.ShippingCity,
+            request.ShippingState,
+            request.ShippingCountry
+        );
+
+        if (result.IsFailed) return Result.Fail(result.Errors);
+
+        Order order = result.Value;
+
+        await _orderRepository.CreateAsync(order);
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return Result.Ok();
+    }
+}
---
+++ backend/Ecommerce.Application/Features/Orders/Commands/OrderCheckout/OrderCheckoutCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿namespace Ecommerce.Application.Features.Orders.Commands.OrderCheckout;
+
+public class OrderCheckoutCommandValidator : AbstractValidator<OrderCheckoutCommand>
+{
+    public OrderCheckoutCommandValidator()
+    {
+        RuleFor(oc => oc.UserId).NotEmpty().GreaterThan(0);
+        RuleFor(oc => oc.CartItems).NotEmpty();
+        RuleFor(oc => oc.ShippingPostalCode).NotEmpty();
+        RuleFor(oc => oc.ShippingStreetName).NotEmpty();
+        RuleFor(oc => oc.ShippingBuildingNumber).NotEmpty();
+    }
+}
