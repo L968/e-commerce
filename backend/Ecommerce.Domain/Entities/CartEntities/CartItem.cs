8cd1f247eec247ea18c7ba4e856e60f72b3f9ae7
L968 <lucas.c968@gmail.com>
2023-07-09 17:12:39 -0300

    Adding IsSelectedForCheckout property, as well of it's setter method;

--- backend/Ecommerce.Domain/Entities/CartEntities/CartItem.cs
+++ backend/Ecommerce.Domain/Entities/CartEntities/CartItem.cs
@@ -4,22 +4,23 @@ public sealed class CartItem : AuditableEntity
 {
     public int Id { get; private set; }
     public int CartId { get; private set; }
-    public int ProductVariantId { get; private set; }
+    public Guid ProductId { get; private set; }
     public int Quantity { get; private set; }
-    public ProductVariant? ProductVariant { get; set; }
+    public bool IsSelectedForCheckout { get; private set; }
+    public Product Product { get; set; }
 
-    private CartItem(int cartId, int productVariantId, int quantity)
+    private CartItem(int cartId, Guid productId, int quantity)
     {
         CartId = cartId;
-        ProductVariantId = productVariantId;
+        ProductId = productId;
         Quantity = quantity;
     }
 
-    public static Result<CartItem> Create(int cartId, int productVariantId, int quantity)
+    public static Result<CartItem> Create(int cartId, Guid productId, int quantity)
     {
         if (quantity <= 0) return Result.Fail(DomainErrors.CartItem.InvalidQuantity);
 
-        return Result.Ok(new CartItem(cartId, productVariantId, quantity));
+        return Result.Ok(new CartItem(cartId, productId, quantity));
     }
 
     public void IncrementQuantity(int quantity)
@@ -30,5 +31,10 @@ public sealed class CartItem : AuditableEntity
     public void SetQuantity(int quantity)
     {
         Quantity = quantity;
+    }
+
+    public void SetIsSelectedForCheckout(bool isSelectedForCheckout)
+    {
+        IsSelectedForCheckout = isSelectedForCheckout;
     }
 }
