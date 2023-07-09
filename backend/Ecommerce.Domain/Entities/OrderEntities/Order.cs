958d2adea0d6a7720277a8e2094f972e74a157d5
L968 <lucas.c968@gmail.com>
2023-07-09 17:13:27 -0300

    Implementing rich domain model to  the Order entity

--- backend/Ecommerce.Domain/Entities/OrderEntities/Order.cs
+++ backend/Ecommerce.Domain/Entities/OrderEntities/Order.cs
@@ -1,27 +1,34 @@
-﻿namespace Ecommerce.Domain.Entities.OrderEntities;
+﻿using Ecommerce.Domain.Enums;
+
+namespace Ecommerce.Domain.Entities.OrderEntities;
 
 public sealed class Order : AuditableEntity
 {
-    public int Id { get; private set; }
-    public Guid Guid { get; private set; }
+    public Guid Id { get; private set; }
     public int UserId { get; private set; }
     public decimal TotalProducts { get; private set; }
-    public decimal? ShippingCost { get; private set; }
+    public decimal ShippingCost { get; private set; }
     public decimal? Discount { get; private set; }
     public decimal Total { get; private set; }
-    public string ShippingPostalCode { get; private set; }
-    public string ShippingStreetName { get; private set; }
-    public string ShippingBuildingNumber { get; private set; }
+    public string ShippingPostalCode { get; private set; } = "";
+    public string ShippingStreetName { get; private set; } = "";
+    public string ShippingBuildingNumber { get; private set; } = "";
     public string? ShippingComplement { get; private set; }
     public string? ShippingNeighborhood { get; private set; }
     public string? ShippingCity { get; private set; }
     public string? ShippingState { get; private set; }
     public string? ShippingCountry { get; private set; }
+    public ICollection<OrderHistory> OrderHistory { get; private set; }
 
-    public Order(
+    private readonly List<OrderItem> _orderItems = new();
+    public IReadOnlyCollection<OrderItem> OrderItems => _orderItems;
+
+    private Order() { }
+
+    private Order(
         int userId,
         decimal totalProducts,
-        decimal? shippingCost,
+        decimal shippingCost,
         decimal? discount,
         decimal total,
         string shippingPostalCode,
@@ -32,7 +39,9 @@ public sealed class Order : AuditableEntity
         string? shippingCity,
         string? shippingState,
         string? shippingCountry
-    ) {
+    )
+    {
+        Id = Guid.NewGuid();
         UserId = userId;
         TotalProducts = totalProducts;
         ShippingCost = shippingCost;
@@ -48,14 +57,9 @@ public sealed class Order : AuditableEntity
         ShippingCountry = shippingCountry;
     }
 
-    public Order(
-        int id,
-        Guid guid,
+    public static Result<Order> Create(
         int userId,
-        decimal totalProducts,
-        decimal? shippingCost,
-        decimal? discount,
-        decimal total,
+        IEnumerable<CartItem> cartItems,
         string shippingPostalCode,
         string shippingStreetName,
         string shippingBuildingNumber,
@@ -64,51 +68,82 @@ public sealed class Order : AuditableEntity
         string? shippingCity,
         string? shippingState,
         string? shippingCountry
-    ) {
-        Id = id;
-        Guid = guid;
-        UserId = userId;
-        TotalProducts = totalProducts;
-        ShippingCost = shippingCost;
-        Discount = discount;
-        Total = total;
-        ShippingPostalCode = shippingPostalCode;
-        ShippingStreetName = shippingStreetName;
-        ShippingBuildingNumber = shippingBuildingNumber;
-        ShippingComplement = shippingComplement;
-        ShippingNeighborhood = shippingNeighborhood;
-        ShippingCity = shippingCity;
-        ShippingState = shippingState;
-        ShippingCountry = shippingCountry;
+    )
+    {
+        if (userId <= 0)
+        {
+            return Result.Fail(DomainErrors.Order.InvalidUserId);
+        }
+
+        var selectedCartItems = cartItems.Where(ci => ci.IsSelectedForCheckout);
+        if (!selectedCartItems.Any()) return Result.Fail(DomainErrors.Order.EmptyProductList);
+
+
+        decimal total = 0;
+        decimal discount = 0;
+
+        foreach (CartItem cartItem in selectedCartItems)
+        {
+            Product product = cartItem.Product;
+            decimal productPrice = product.Price;
+
+            if (!product.Active) return Result.Fail(DomainErrors.Order.InactiveProduct);
+
+            if (product.Inventory.Stock < cartItem.Quantity) return Result.Fail(DomainErrors.Order.InsufficientStock);
+
+            ProductDiscount? activeProductDiscount = product.Discounts.Where(d => d.IsCurrentlyActive()).FirstOrDefault();
+
+            if (activeProductDiscount is not null)
+            {
+                switch (activeProductDiscount.DiscountUnit)
+                {
+                    case DiscountUnit.Percentage:
+                        productPrice = productPrice * activeProductDiscount.DiscountValue / 100;
+                        discount += product.Price - productPrice;
+                        break;
+                    case DiscountUnit.FixedAmount:
+                        productPrice -= activeProductDiscount.DiscountValue;
+                        discount += activeProductDiscount.DiscountValue;
+                        break;
+                    default:
+                        return Result.Fail(DomainErrors.Order.DiscountUnitNotImplemented);
+                }
+            }
+
+            var result = product.Inventory.ReduceStock(cartItem.Quantity);
+            if (result.IsFailed) return result;
+
+            total += productPrice;
+        }
+
+        decimal shippingCost = CalculateShippingCost(selectedCartItems.Count());
+
+        var order = new Order(
+            userId,
+            totalProducts: selectedCartItems.Count(),
+            shippingCost,
+            discount,
+            total,
+            shippingPostalCode,
+            shippingStreetName,
+            shippingBuildingNumber,
+            shippingComplement,
+            shippingNeighborhood,
+            shippingCity,
+            shippingState,
+            shippingCountry
+        );
+
+        // insere historico
+        // insere pedido
+        // limpar carrinho
+        return order;
     }
 
-    public void Update(
-        int userId,
-        decimal totalProducts,
-        decimal? shippingCost,
-        decimal? discount,
-        decimal total,
-        string shippingPostalCode,
-        string shippingStreetName,
-        string shippingBuildingNumber,
-        string? shippingComplement,
-        string? shippingNeighborhood,
-        string? shippingCity,
-        string? shippingState,
-        string? shippingCountry
-    ) {
-        UserId = userId;
-        TotalProducts = totalProducts;
-        ShippingCost = shippingCost;
-        Discount = discount;
-        Total = total;
-        ShippingPostalCode = shippingPostalCode;
-        ShippingStreetName = shippingStreetName;
-        ShippingBuildingNumber = shippingBuildingNumber;
-        ShippingComplement = shippingComplement;
-        ShippingNeighborhood = shippingNeighborhood;
-        ShippingCity = shippingCity;
-        ShippingState = shippingState;
-        ShippingCountry = shippingCountry;
+    private static decimal CalculateShippingCost(int orderItems)
+    {
+        return orderItems * 10;
     }
+
+    // (Application) Verifica se produto existe
 }
--- backend/Ecommerce.Domain/Entities/OrderEntities/OrderItem.cs
+++ backend/Ecommerce.Domain/Entities/OrderEntities/OrderItem.cs
@@ -3,10 +3,11 @@
 public sealed class OrderItem
 {
     public int Id { get; private set; }
-    public int OrderId { get; private set; }
+    public Guid OrderId { get; private set; }
+    public Guid ProductId { get; set; }
     public string ProductName { get; private set; }
     public string ProductSku { get; private set; }
     public string? ProductImagePath { get; private set; }
-    public decimal? ProductDiscount { get; private set; }
     public decimal ProductPrice { get; private set; }
+    public decimal? ProductDiscount { get; private set; }
 }
