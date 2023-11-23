3e0195008b677faf543c41ae3145ba350d734f2c
L968 <lucas.c968@gmail.com>
2023-11-23 15:43:21 -0300

    Adding orderCheckoutDto child validation

--- backend/Ecommerce.Application/DTOs/OrderCheckout/OrderCheckoutDtoValidator.cs
+++ backend/Ecommerce.Application/DTOs/OrderCheckout/OrderCheckoutDtoValidator.cs
@@ -6,8 +6,19 @@ public class OrderCheckoutDtoValidator : AbstractValidator<OrderCheckoutDto>
     {
         RuleFor(oc => oc.UserId).NotEmpty().GreaterThan(0);
         RuleFor(oc => oc.CartItems).NotEmpty();
+        RuleForEach(oc => oc.CartItems).SetValidator(new OrderCheckoutCartItemDtoValidator());
         RuleFor(oc => oc.ShippingPostalCode).NotEmpty();
         RuleFor(oc => oc.ShippingStreetName).NotEmpty();
         RuleFor(oc => oc.ShippingBuildingNumber).NotEmpty();
+    }
+
+    public class OrderCheckoutCartItemDtoValidator : AbstractValidator<OrderCheckoutCartItemDto>
+    {
+        public OrderCheckoutCartItemDtoValidator()
+        {
+            RuleFor(ci => ci.CartId).NotEmpty().GreaterThan(0);
+            RuleFor(ci => ci.ProductCombinationId).NotEmpty();
+            RuleFor(ci => ci.Quantity).NotEmpty().GreaterThan(0);
+        }
     }
 }
