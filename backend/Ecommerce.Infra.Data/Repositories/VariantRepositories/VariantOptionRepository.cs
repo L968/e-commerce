23efe4d2ebc814287653fdf7b83a640d33d86e10
L968 <lucas.c968@gmail.com>
2023-11-25 20:12:54 -0300

    Adding VariantOptionRepository

---
+++ backend/Ecommerce.Infra.Data/Repositories/VariantRepositories/VariantOptionRepository.cs
@@ -0,0 +1,44 @@
+﻿using Ecommerce.Domain.Entities.VariantEntities;
+using Ecommerce.Domain.Repositories.VariantRepositories;
+
+namespace Ecommerce.Infra.Data.Repositories.VariantRepositories;
+
+public class VariantOptionRepository : IVariantOptionRepository
+{
+    private readonly AppDbContext _context;
+
+    public VariantOptionRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<VariantOption>> GetAllAsync()
+    {
+        return await _context.VariantOptions
+            .Include(v => v.Variant)
+            .ToListAsync();
+    }
+
+    public async Task<VariantOption?> GetByIdAsync(int id)
+    {
+        return await _context.VariantOptions
+            .Include(v => v.Variant)
+            .FirstOrDefaultAsync(v => v.Id == id);
+    }
+
+    public VariantOption Create(VariantOption variantOption)
+    {
+        _context.VariantOptions.Add(variantOption);
+        return variantOption;
+    }
+
+    public void Update(VariantOption variantOption)
+    {
+        _context.VariantOptions.Update(variantOption);
+    }
+
+    public void Delete(VariantOption variantOption)
+    {
+        _context.VariantOptions.Remove(variantOption);
+    }
+}
