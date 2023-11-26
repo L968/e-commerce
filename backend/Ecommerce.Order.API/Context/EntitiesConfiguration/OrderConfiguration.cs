8ced4e3a9f63b8f17c369bf2cf4c6f90ef64ebd6
L968 <lucas.c968@gmail.com>
2023-11-26 00:07:41 -0300

    Adding order context configuration

---
+++ backend/Ecommerce.Order.API/Context/EntitiesConfiguration/OrderConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Order.API.Context.EntitiesConfiguration
+{
+    public class OrderConfiguration : IEntityTypeConfiguration<Domain.Entities.OrderEntities.Order>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Domain.Entities.OrderEntities.Order> builder)
+        {
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.TotalAmount).HasPrecision(65, 2);
+            builder.Property(p => p.ShippingCost).HasPrecision(65, 2);
+            builder.Property(p => p.Discount).HasPrecision(65, 2);
+        }
+    }
+}
---
+++ backend/Ecommerce.Order.API/Context/EntitiesConfiguration/OrderItemConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Order.API.Context.EntitiesConfiguration
+{
+    public class OrderItemConfiguration : IEntityTypeConfiguration<Domain.Entities.OrderEntities.OrderItem>
+    {
+        public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Domain.Entities.OrderEntities.OrderItem> builder)
+        {
+            builder.HasKey(oi => oi.Id);
+            builder.Property(oi => oi.ProductUnitPrice).HasPrecision(65, 2);
+            builder.Property(oi => oi.ProductDiscount).HasPrecision(65, 2);
+        }
+    }
+}
