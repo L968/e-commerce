fe434a44209c93ffe47aaf4b5bbb71b67edbab44
L968 <lucas.c968@gmail.com>
2023-12-09 22:09:30 -0300

    Moving DTO files to DTO folder

--- backend/Ecommerce.Application/Features/ProductCategories/Queries/GetProductCategoryDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Ecommerce.Application.Features.ProductCategories.Queries;
-
-public record GetProductCategoryDto
-{
-    public Guid? Guid { get; set; }
-    public string? Name { get; set; }
-    public string? Description { get; set; }
-}
--- backend/Ecommerce.Application/Features/Products/Queries/GetProductImageDto.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Ecommerce.Application.Features.Products.Queries;
-
-public record GetProductImageDto
-{
-    public int Id { get; set; }
-    public string? ImagePath { get; set; }
-}
