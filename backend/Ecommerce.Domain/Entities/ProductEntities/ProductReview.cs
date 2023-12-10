271d2f4c32e2dcc21ab88686c5b49862d1044ed1
L968 <lucas.c968@gmail.com>
2023-12-09 22:13:55 -0300

    Updating product review properties

--- backend/Ecommerce.Domain/Entities/ProductEntities/ProductReview.cs
+++ backend/Ecommerce.Domain/Entities/ProductEntities/ProductReview.cs
@@ -3,9 +3,10 @@
 public sealed class ProductReview : AuditableEntity
 {
     public int Id { get; private set; }
-    public int UserId { get; private set; }
-    public int ProductId { get; private set; }
+    public int? UserId { get; private set; }
+    public Guid ProductId { get; private set; }
     public int Rating { get; private set; }
     public string? Description { get; private set; }
-    public bool Anonymous { get; private set; }
+
+    public Product? Product { get; private set; }
 }
