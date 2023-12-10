da5851af91bf11b13f0a1dac65685cf3b84c2450
L968 <lucas.c968@gmail.com>
2023-12-09 22:41:48 -0300

    Moving frontend project to child folder

--- frontend_vite/src/graphql/client.tsx
+++
@@ -1,9 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-import { RestLink } from "apollo-link-rest";
-
-const link = new RestLink({ uri: "https://localhost:7283/" });
-
-export const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  link,
-});
--- frontend_vite/src/pages/Main/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Container } from "./styles"
-
-export const Main = (): JSX.Element => {
-  return (
-    <Container>
-      <h1>MAIN PAGE</h1>
-    </Container>
-  );
-}
