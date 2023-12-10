76d3e095ed0192209c9394569b1f4c39eb1c9654
L968 <lucas.c968@gmail.com>
2023-12-09 22:06:04 -0300

    Addin user secrets to API project

---
+++ backend/Ecommerce.Application/Config.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Ecommerce.Application;
+
+public class Config
+{
+    private readonly IConfiguration _configuration;
+
+    public static string AzureStorageConnectionString { get; set; } = "";
+    public static string AzureStorageContainer { get; set; } = "";
+
+    public Config(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public void Init()
+    {
+        AzureStorageConnectionString = _configuration.GetValue<string>("AzureStorage:ConnectionString") ?? throw new ArgumentNullException("AzureStorage:ConnectionString");
+        AzureStorageContainer = _configuration.GetValue<string>("AzureStorage:Container") ?? throw new ArgumentNullException("AzureStorage:Container");
+    }
+}
