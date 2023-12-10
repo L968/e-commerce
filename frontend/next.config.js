1e74d7f54ea69832c99aa7f90adb9b977f99c720
L968 <lucas.c968@gmail.com>
2023-12-09 22:29:25 -0300

    Adding image src hostname configuration

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images:{
+        remotePatterns: [{
+            protocol: 'https',
+            hostname: process.env.IMAGE_HOSTNAME
+        }]
+    },
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/home',
+                permanent: true
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig
