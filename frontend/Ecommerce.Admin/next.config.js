2354274612ca8e3b2a1a9887a67f88edb5fe945a
L968 <lucas.c968@gmail.com>
2023-12-18 10:19:08 -0300

    Adding azure url to nextconfig

--- frontend/Ecommerce.Admin/next.config.js
+++ frontend/Ecommerce.Admin/next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images:{
+        remotePatterns: [{
+            protocol: 'https',
+            hostname: process.env.IMAGE_HOSTNAME
+        }]
+    },
     async redirects() {
         return [
             {
