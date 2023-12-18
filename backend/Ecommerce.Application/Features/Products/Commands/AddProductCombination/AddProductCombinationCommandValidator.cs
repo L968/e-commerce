76daa1524f0ebcd863b6121c540d78140d151f88
L968 <lucas.c968@gmail.com>
2023-12-18 10:17:58 -0300

    Adding validation to images prop

--- backend/Ecommerce.Application/Features/Products/Commands/AddProductCombination/AddProductCombinationCommandValidator.cs
+++ backend/Ecommerce.Application/Features/Products/Commands/AddProductCombination/AddProductCombinationCommandValidator.cs
@@ -8,6 +8,7 @@ public class AddProductCombinationCommandValidator : AbstractValidator<AddProduc
         RuleFor(p => p.Sku).NotEmpty();
         RuleFor(p => p.Price).NotEmpty().GreaterThan(0);
         RuleFor(p => p.Stock).NotEmpty().GreaterThanOrEqualTo(0);
+        RuleFor(p => p.Images).NotEmpty();
         RuleFor(p => p.Length).NotEmpty().GreaterThan(0);
         RuleFor(p => p.Width).NotEmpty().GreaterThan(0);
         RuleFor(p => p.Height).NotEmpty().GreaterThan(0);
