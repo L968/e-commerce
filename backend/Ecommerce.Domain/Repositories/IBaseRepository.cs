cd90a50d92ab57a46a1085a634127a11da502081
L968 <lucas.c968@gmail.com>
2023-12-19 10:37:17 -0300

    Adding base repository

---
+++ backend/Ecommerce.Domain/Repositories/IBaseRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Ecommerce.Domain.Repositories;
+
+public interface IBaseRepository<T>
+{
+    T Create(T entity);
+    void Update(T entity);
+    void Delete(T entity);
+}
---
+++ backend/Ecommerce.Infra.Data/Repositories/BaseRepository.cs
@@ -0,0 +1,25 @@
+﻿namespace Ecommerce.Infra.Data.Repositories;
+
+public class BaseRepository<T> where T : class
+{
+    protected readonly AppDbContext _context;
+
+    public BaseRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public T Create(T entity)
+    {
+        _context.Set<T>().Add(entity);
+        return entity;
+    }
+    public void Update(T entity)
+    {
+        _context.Set<T>().Update(entity);
+    }
+    public void Delete(T entity)
+    {
+        _context.Set<T>().Remove(entity);
+    }
+}
