2c04edf4baa119653a816ad24a4ca5733cc3fbc6
L968 <lucas.c968@gmail.com>
2024-01-12 14:24:06 -0300

    Adding cart configuration

--- backend/Ecommerce.Infra.Data/EntitiesConfiguration/CartConfiguration.cs
+++ backend/Ecommerce.Infra.Data/EntitiesConfiguration/CartConfiguration.cs
@@ -4,9 +4,9 @@ public class CartConfiguration : IEntityTypeConfiguration<Cart>
 {
     public void Configure(EntityTypeBuilder<Cart> builder)
     {
-        //builder
-        //    .HasMany(cart => cart.CartItems)
-        //    .WithOne(cartItems => cartItems.Cart)
-        //    .HasForeignKey(cartItems => cartItems.CartId);
+        builder
+            .HasMany(cart => cart.CartItems)
+            .WithOne(cartItems => cartItems.Cart)
+            .HasForeignKey(cartItems => cartItems.CartId);
     }
 }
