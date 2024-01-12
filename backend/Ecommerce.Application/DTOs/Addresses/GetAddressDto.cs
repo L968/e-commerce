30c6659d3cd98c66ee663b37ea00318c6aafb5c5
L968 <lucas.c968@gmail.com>
2024-01-12 14:15:43 -0300

    Updating address dto

--- backend/Ecommerce.Application/DTOs/Addresses/GetAddressDto.cs
+++ backend/Ecommerce.Application/DTOs/Addresses/GetAddressDto.cs
@@ -1,17 +1,18 @@
 ﻿namespace Ecommerce.Application.DTOs.Addresses;
 
-public record GetAddressDto
+public class GetAddressDto
 {
     public int? Id { get; set; }
-    public string? RecipientFullName { get; set; }
-    public string? RecipientPhoneNumber { get; set; }
-    public string? PostalCode { get; set; }
-    public string? StreetName { get; set; }
-    public string? BuildingNumber { get; set; }
+    public int UserId { get; set; }
+    public string RecipientFullName { get; set; } = "";
+    public string RecipientPhoneNumber { get; set; } = "";
+    public string PostalCode { get; set; } = "";
+    public string StreetName { get; set; } = "";
+    public string BuildingNumber { get; set; } = "";
     public string? Complement { get; set; }
     public string? Neighborhood { get; set; }
-    public string? City { get; set; }
-    public string? State { get; set; }
-    public string? Country { get; set; }
+    public string City { get; set; } = "";
+    public string State { get; set; } = "";
+    public string Country { get; set; } = "";
     public string? AdditionalInformation { get; set; }
 }
