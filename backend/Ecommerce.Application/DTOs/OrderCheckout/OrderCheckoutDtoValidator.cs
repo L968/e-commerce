bb279adbd0a07c84363ae711d5bf0ae9264aa53e
L968 <lucas.c968@gmail.com>
2024-01-12 14:16:17 -0300

    Updating order dto validation

--- backend/Ecommerce.Application/DTOs/OrderCheckout/OrderCheckoutDtoValidator.cs
+++ backend/Ecommerce.Application/DTOs/OrderCheckout/OrderCheckoutDtoValidator.cs
@@ -5,18 +5,15 @@ public class OrderCheckoutDtoValidator : AbstractValidator<OrderCheckoutDto>
     public OrderCheckoutDtoValidator()
     {
         RuleFor(oc => oc.UserId).NotEmpty().GreaterThan(0);
-        RuleFor(oc => oc.CartItems).NotEmpty();
-        RuleForEach(oc => oc.CartItems).SetValidator(new OrderCheckoutCartItemDtoValidator());
-        RuleFor(oc => oc.ShippingPostalCode).NotEmpty();
-        RuleFor(oc => oc.ShippingStreetName).NotEmpty();
-        RuleFor(oc => oc.ShippingBuildingNumber).NotEmpty();
+        RuleFor(oc => oc.OrderCheckoutItems).NotEmpty();
+        RuleForEach(oc => oc.OrderCheckoutItems).SetValidator(new OrderCheckoutCartItemDtoValidator());
+        RuleFor(oc => oc.ShippingAddressId).NotEmpty();
     }
 
-    public class OrderCheckoutCartItemDtoValidator : AbstractValidator<OrderCheckoutCartItemDto>
+    public class OrderCheckoutCartItemDtoValidator : AbstractValidator<OrderCheckoutItemDto>
     {
         public OrderCheckoutCartItemDtoValidator()
         {
-            RuleFor(ci => ci.CartId).NotEmpty().GreaterThan(0);
             RuleFor(ci => ci.ProductCombinationId).NotEmpty();
             RuleFor(ci => ci.Quantity).NotEmpty().GreaterThan(0);
         }
