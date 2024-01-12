e52e94c60f96f110d58e4bde222acb28b7b005d3
L968 <lucas.c968@gmail.com>
2024-01-12 14:24:33 -0300

    adding order configurations

--- backend/Ecommerce.Order.API/Context/EntitiesConfiguration/OrderConfiguration.cs
+++ backend/Ecommerce.Order.API/Context/EntitiesConfiguration/OrderConfiguration.cs
@@ -6,6 +6,16 @@ namespace Ecommerce.Order.API.Context.EntitiesConfiguration
     {
         public void Configure(Microsoft.EntityFrameworkCore.Metadata.Builders.EntityTypeBuilder<Domain.Entities.OrderEntities.Order> builder)
         {
+            builder
+                .HasMany(o => o.Items)
+                .WithOne(oi => oi.Order)
+                .HasForeignKey(oi => oi.OrderId);
+
+            builder
+                .HasMany(o => o.History)
+                .WithOne(oh => oh.Order)
+                .HasForeignKey(oh => oh.OrderId);
+
             builder.HasKey(p => p.Id);
             builder.Property(p => p.TotalAmount).HasPrecision(65, 2);
             builder.Property(p => p.ShippingCost).HasPrecision(65, 2);
