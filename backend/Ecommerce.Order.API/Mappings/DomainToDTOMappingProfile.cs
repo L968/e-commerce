60d44ca780a2f0085638409e6826056d29b938db
L968 <lucas.c968@gmail.com>
2024-01-14 19:05:58 -0300

    Adding new order dto mapping

--- backend/Ecommerce.Order.API/Mappings/DomainToDTOMappingProfile.cs
+++ backend/Ecommerce.Order.API/Mappings/DomainToDTOMappingProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Ecommerce.Application.DTOs;
+using Ecommerce.Order.API.Utils;
 
 namespace Ecommerce.Order.API.Mappings;
 
@@ -8,7 +9,8 @@ public class DomainToDTOMappingProfile : Profile
     public DomainToDTOMappingProfile()
     {
         CreateMap<Domain.Entities.OrderEntities.Order, OrderDto>()
-            .ForMember(dto => dto.ShippingAddress, opt => opt.MapFrom(o => $"{o.ShippingStreetName} {o.ShippingBuildingNumber}, {o.ShippingNeighborhood}, {o.ShippingCity}, {o.ShippingState}")); ;
+            .ForMember(dto => dto.ShippingAddress, opt => opt.MapFrom(o => $"{o.ShippingStreetName} {o.ShippingBuildingNumber}, {o.ShippingNeighborhood}, {o.ShippingCity}, {o.ShippingState}"))
+            .ForMember(dto => dto.Status, opt => opt.MapFrom(o => StringManipulationUtils.AddSpacesBeforeUpperCase(o.Status.ToString())));
 
         CreateMap<Domain.Entities.OrderEntities.OrderItem, OrderItemDto>();
         CreateMap<Domain.Entities.OrderEntities.OrderHistory, OrderHistoryDto>();
