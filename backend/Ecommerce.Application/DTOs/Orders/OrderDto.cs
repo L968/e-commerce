01fc9c6bde9fb2780276b4d1c6d7911e96c65f96
L968 <lucas.c968@gmail.com>
2024-01-14 19:05:27 -0300

    Adding new fields to dto

--- backend/Ecommerce.Application/DTOs/Orders/OrderDto.cs
+++ backend/Ecommerce.Application/DTOs/Orders/OrderDto.cs
@@ -5,12 +5,13 @@ namespace Ecommerce.Application.DTOs;
 public record OrderDto
 {
     public Guid Id { get; set; }
-    public OrderStatus Status { get; set; }
+    public string Status { get; set; } = "";
     public decimal ShippingCost { get; set; }
     public decimal? Discount { get; set; }
     public decimal TotalAmount { get; set; }
     public string ShippingAddress { get; set; } = "";
+    public DateTime CreatedAt { get; set; }
 
-    public List<OrderHistoryDto> History = [];
-    public List<OrderItemDto> Items = [];
+    public List<OrderHistoryDto> History { get; set; } = [];
+    public List<OrderItemDto> Items { get; set; } = [];
 }
--- backend/Ecommerce.Application/DTOs/Orders/OrderItemDto.cs
+++ backend/Ecommerce.Application/DTOs/Orders/OrderItemDto.cs
@@ -5,9 +5,10 @@ public record OrderItemDto
     public int Id { get; set; }
     public Guid ProductCombinationId { get; set; }
     public int Quantity { get; set; }
-    public string ProductName { get; set; }
-    public string ProductSku { get; set; }
+    public string ProductName { get; set; } = "";
+    public string ProductSku { get; set; } = "";
     public string? ProductImagePath { get; set; }
     public decimal ProductUnitPrice { get; set; }
     public decimal? ProductDiscount { get; set; }
+    public string Description { get; set; } = "";
 }
