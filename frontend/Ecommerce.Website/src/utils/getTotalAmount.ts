3b314d0172681efbff45c6b3060b618f5d203755
Lucas Constantino <lucas.c968@gmail.com>
2024-03-10 14:22:23 -0300

    Adding getTotalAmount method

---
+++ frontend/Ecommerce.Website/src/utils/getTotalAmount.ts
@@ -0,0 +1,10 @@
+interface Item {
+    product: {
+        discountedPrice: number;
+    };
+    quantity: number;
+}
+
+export default function getTotalAmount(items: Item[]): number {
+    return items.reduce((total, item) =>  total + item.product.discountedPrice * item.quantity, 0);
+}
