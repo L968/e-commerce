379ea9c9f1dd64a87793226f14828f94d827a320
Lucas Constantino <lucas.c968@gmail.com>
2024-03-25 09:35:53 -0300

    Implementing delete product button

--- frontend/Ecommerce.Admin/src/components/ProductCard/index.tsx
+++ frontend/Ecommerce.Admin/src/components/ProductCard/index.tsx
@@ -1,15 +1,26 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import api from '@/services/api';
+import { useState } from 'react';
 import { Button } from '@mui/material';
+import { toast } from 'react-toastify';
+import { LoadingButton } from '@mui/lab';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
-import { ActionButtons, CardInfo, Container, Price, ProductName } from './styles';
 import { Product } from '@/interfaces/api/responses/GetProductListResponse';
+import { ActionButtons, CardInfo, Container, Price, ProductName } from './styles';
 
 export default function ProductCard(product: Product) {
     const { id, name, discountedPrice: price, imageSource } = product;
+    const [loading, setLoading] = useState<boolean>(false);
 
-    function handleDelete(e: React.MouseEvent) {
+    function handleDelete() {
+        setLoading(true);
+
+        api.delete('/product/' + id)
+            .then(response => toast.success("Product deleted"))
+            .catch(error => toast.error("Error 500"))
+            .finally(() => setLoading(false));
     }
 
     return (
@@ -37,17 +48,17 @@ export default function ProductCard(product: Product) {
                         </Button>
                     </Link>
 
-
-                    <Button
+                    <LoadingButton
                         sx={{ color: 'red' }}
                         variant="contained"
                         size='small'
                         color="secondary"
                         onClick={handleDelete}
                         startIcon={<DeleteIcon />}
+                        loading={loading}
                     >
                         Delete
-                    </Button>
+                    </LoadingButton>
                 </ActionButtons>
             </CardInfo>
         </Container>
