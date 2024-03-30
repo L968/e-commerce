f1aef7a3e1e41eb6f6ef7c16f96e943254898729
Lucas Constantino <lucas.c968@gmail.com>
2024-03-29 21:17:34 -0300

    Fixing tests

--- backend/Ecommerce.Domain.UnitTests/SampleData.cs
+++ backend/Ecommerce.Domain.UnitTests/SampleData.cs
@@ -9,53 +9,98 @@ namespace Ecommerce.Domain.UnitTests
         {
             var variantIds = new List<Guid>();
 
-            return new List<ProductCategory>
-            {
+            return
+            [
                 ProductCategory.Create("Electronics", "Electronic devices and accessories", variantIds).Value,
                 ProductCategory.Create("Clothing", "Fashionable clothing items", variantIds).Value,
                 ProductCategory.Create("Home and Kitchen", "Household items and kitchenware", variantIds).Value
-            };
+            ];
         }
 
         public static List<Product> GetProducts()
         {
             var product1 = Product.Create("Smartphone XYZ", "High-end smartphone with advanced features", true, true, Guid.NewGuid()).Value;
-            var productCombination1 = ProductCombination.Create(product1.Id, "Color: Black, Storage: 128GB", "XYZ-128GB-BLACK", 1000, 100, 5.5f, 2.8f, 0.3f, 0.5f, new List<string>() { "/images/smartphone_xyz_black.jpg" }).Value;
+            var productCombination1 = ProductCombination.Create(
+                productId: product1.Id, 
+                existingCombinations: product1.Combinations, 
+                variantOptions: [], 
+                sku: "XYZ-128GB-BLACK", 
+                price: 1000, 
+                stock: 100, 
+                length: 5.5f, 
+                width: 2.8f, 
+                height: 0.3f, 
+                weight: 0.5f, 
+                imagePaths: ["/images/smartphone_xyz_black.jpg"]
+            ).Value;
             SetPrivateProperty(productCombination1, "Product", product1);
             product1.Combinations.Add(productCombination1);
 
             var product2 = Product.Create("Laptop ABC", "Powerful laptop for professional use", true, true, Guid.NewGuid()).Value;
-            var productCombination2 = ProductCombination.Create(product2.Id, "Model: Pro, Storage: 512GB", "ABC-512GB-PRO", 1500, 50, 14.0f, 9.8f, 0.7f, 2.0f, new List<string>() { "/images/laptop_abc_pro.jpg" }).Value;
+            var productCombination2 = ProductCombination.Create(
+                productId: product2.Id,
+                existingCombinations: product2.Combinations,
+                variantOptions: [],
+                sku: "ABC-512GB-PRO",
+                price: 1500,
+                stock: 50,
+                length: 14.0f,
+                width: 9.8f,
+                height: 0.7f,
+                weight: 2.0f,
+                imagePaths: ["/images/laptop_abc_pro.jpg"]
+            ).Value;
 
             SetPrivateProperty(productCombination2, "Product", product2);
             product2.Combinations.Add(productCombination2);
 
             var product3 = Product.Create("Men\'s Jeans", "Classic blue jeans for men", true, true, Guid.NewGuid()).Value;
 
-            var productCombination3 = ProductCombination.Create(product3.Id, "Size: 34, Color: Blue", "JEANS-34-BLUE", 50, 200, 1, 1, 1, 0.7f, new List<string>() { "/images/mens_jeans_blue.jpg" }).Value;
+            var productCombination3 = ProductCombination.Create(
+                productId: product3.Id,
+                existingCombinations: product3.Combinations,
+                variantOptions: [],
+                sku: "JEANS-34-BLUE",
+                price: 50,
+                stock: 200,
+                length: 1,
+                width: 1,
+                height: 1,
+                weight: 0.7f,
+                imagePaths: ["/images/mens_jeans_blue.jpg"]
+            ).Value;
             SetPrivateProperty(productCombination3, "Product", product3);
             product3.Combinations.Add(productCombination3);
 
             var product4 = Product.Create("Cookware Set", "Complete set of non-stick cookware", true, true, Guid.NewGuid()).Value;
-            var productCombination4 = ProductCombination.Create(product4.Id, "Set: 10-piece, Material: Non-stick", "COOKWARE-10PC-NONSTICK", 200, 30, 1, 1, 1, 0.5f, new List<string>() { "/images/cookware_set_nonstick.jpg" }).Value;
+            var productCombination4 = ProductCombination.Create(
+                productId: product4.Id,
+                existingCombinations: product4.Combinations,
+                variantOptions: [],
+                sku: "COOKWARE-10PC-NONSTICK",
+                price: 200,
+                stock: 30,
+                length: 1,
+                width: 1,
+                height: 1,
+                weight: 0.5f,
+                imagePaths: ["/images/cookware_set_nonstick.jpg"]
+            ).Value;
             SetPrivateProperty(productCombination4, "Product", product4);
             product4.Combinations.Add(productCombination4);
 
-            return new List<Product>
-            {
+            return
+            [
                 product1,
                 product2,
                 product3,
                 product4
-            };
+            ];
         }
 
         public static List<Cart> GetCarts()
         {
-            return new List<Cart>()
-            {
-                new Cart(1)
-            };
+            return [new Cart(1)];
         }
 
         public static List<CartItem> GetCartItems()
@@ -63,12 +108,12 @@ namespace Ecommerce.Domain.UnitTests
             var carts = GetCarts();
             var products = GetProducts();
 
-            return new List<CartItem>()
-            {
+            return
+            [
                 CartItem.Create(carts[0].Id, products[0].Combinations[0].Id, 2, true, products[0].Combinations[0]).Value,
                 CartItem.Create(carts[0].Id, products[1].Combinations[0].Id, 1, true, products[1].Combinations[0]).Value,
                 CartItem.Create(carts[0].Id, products[2].Combinations[0].Id, 3, true, products[2].Combinations[0]).Value
-            };
+            ];
         }
 
         private static void SetPrivateProperty(object instance, string propertyName, object value)
