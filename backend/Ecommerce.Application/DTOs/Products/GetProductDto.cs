01936e685750dda9dbef786394afda36a3956eb6
Lucas Constantino <lucas.c968@gmail.com>
2024-04-17 20:35:20 -0300

    Adding active property to getProductDto

--- backend/Ecommerce.Application/DTOs/Products/GetProductDto.cs
+++ backend/Ecommerce.Application/DTOs/Products/GetProductDto.cs
@@ -7,6 +7,7 @@ public record GetProductDto
     public Guid Id { get; set; }
     public string Name { get; set; } = "";
     public string Description { get; set; } = "";
+    public bool Active { get; set; }
     public float Rating { get; set; }
 
     public List<GetProductCombinationDto> Combinations { get; set; } = [];
