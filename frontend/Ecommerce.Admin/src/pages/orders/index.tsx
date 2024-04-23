51a9eab68c63640f4990a570f045a972074903bd
Lucas Constantino <lucas.c968@gmail.com>
2024-04-22 22:28:48 -0300

    Implementing first requests to the order page

--- frontend/Ecommerce.Admin/src/pages/orders/index.tsx
+++ frontend/Ecommerce.Admin/src/pages/orders/index.tsx
@@ -1,7 +1,29 @@
+import apiOrder from '@/services/apiOrder';
+import { toast } from 'react-toastify';
+import { useEffect, useState } from 'react';
+import { Container, Main } from './styles';
+import { Typography } from '@mui/material';
+
 export default function Orders() {
+    const [orders, setOrders] = useState();
+    const [loading, setLoading] = useState<boolean>(true);
+
+    useEffect(() => {
+        apiOrder.get('/order/all')
+            .then(response => setOrders(response.data))
+            .catch(error => toast.error("Error 500"))
+            .finally(() => setLoading(false));
+    }, []);
+
+    if (!loading && !orders) {
+        return;
+    }
+
     return (
-        <main>
-            Orders
-        </main>
+        <Main>
+            <Typography variant='h1'>Orders</Typography>
+            <Container>
+            </Container>
+        </Main>
     )
 }
--- frontend/Ecommerce.Admin/src/pages/orders/styles.tsx
+++ frontend/Ecommerce.Admin/src/pages/orders/styles.tsx
@@ -1,4 +1,13 @@
 import { styled } from '@mui/system';
+import Paper from '@mui/material/Paper';
 
 export const Main = styled('main')(({ theme }) => ({
 }))
+
+
+export const Container = styled(Paper)({
+    display: 'flex',
+    flexDirection: 'column',
+    padding: '18px',
+    gap: '20px',
+})
