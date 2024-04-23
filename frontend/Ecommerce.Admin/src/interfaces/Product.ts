536cd3147dc80d39f93a4273730c31893324ec95
Lucas Constantino <lucas.c968@gmail.com>
2024-04-23 20:25:39 -0300

    Moving product interface to another file

---
+++ frontend/Ecommerce.Admin/src/interfaces/Product.ts
@@ -0,0 +1,10 @@
+export default interface Product {
+    id: string;
+    name: string;
+    categoryName: string;
+    originalPrice: number;
+    discountedPrice: number;
+    imageSource: string;
+    rating: number;
+    reviewsCount: number;
+}
--- frontend/Ecommerce.Admin/src/interfaces/api/responses/GetProductListResponse.ts
+++ frontend/Ecommerce.Admin/src/interfaces/api/responses/GetProductListResponse.ts
@@ -1,18 +1,9 @@
+import Product from '@/interfaces/Product';
+
 export default interface GetProductListResponse {
     page: number
     itemsPerPage: number
     totalItems: number
     totalPages: number
     items: Product[]
-}
-
-export interface Product {
-    id: string;
-    name: string;
-    categoryName: string;
-    originalPrice: number;
-    discountedPrice: number;
-    imageSource: string;
-    rating: number;
-    reviewsCount: number;
 }
