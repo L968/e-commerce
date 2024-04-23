fd8228b4e685fd7b31a8a805d4f1281ceb4f3f1f
Lucas Constantino <lucas.c968@gmail.com>
2024-04-22 22:27:37 -0300

    Adding confirmation dialog when deleting product

--- frontend/Ecommerce.Admin/src/components/ProductCard/index.tsx
+++ frontend/Ecommerce.Admin/src/components/ProductCard/index.tsx
@@ -2,24 +2,25 @@ import Image from 'next/image';
 import Link from 'next/link';
 import api from '@/services/api';
 import { useState } from 'react';
-import { Button } from '@mui/material';
 import { toast } from 'react-toastify';
 import { LoadingButton } from '@mui/lab';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { Product } from '@/interfaces/api/responses/GetProductListResponse';
 import { ActionButtons, CardInfo, Container, Price, ProductName } from './styles';
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from '@mui/material';
 
 export default function ProductCard(product: Product) {
     const { id, name, discountedPrice: price, imageSource } = product;
     const [loading, setLoading] = useState<boolean>(false);
+    const [openDeleteDialog, setOpenDeleteDialog] = useState<boolean>(false);
 
     function handleDelete() {
         setLoading(true);
 
         api.delete('/product/' + id)
-            .then(response => toast.success("Product deleted"))
-            .catch(error => toast.error("Error 500"))
+            .then(response => toast.success('Product deleted'))
+            .catch(error => toast.error('Error 500'))
             .finally(() => setLoading(false));
     }
 
@@ -39,8 +40,8 @@ export default function ProductCard(product: Product) {
                 <ActionButtons>
                     <Link href={`/products/${id}/edit`}>
                         <Button
-                            variant="contained"
-                            color="secondary"
+                            variant='contained'
+                            color='secondary'
                             size='small'
                             startIcon={<EditIcon />}
                         >
@@ -50,10 +51,10 @@ export default function ProductCard(product: Product) {
 
                     <LoadingButton
                         sx={{ color: 'red' }}
-                        variant="contained"
+                        variant='contained'
                         size='small'
-                        color="secondary"
-                        onClick={handleDelete}
+                        color='secondary'
+                        onClick={() => setOpenDeleteDialog(true)}
                         startIcon={<DeleteIcon />}
                         loading={loading}
                     >
@@ -61,6 +62,31 @@ export default function ProductCard(product: Product) {
                     </LoadingButton>
                 </ActionButtons>
             </CardInfo>
+
+            <Dialog open={openDeleteDialog} onClose={() => setOpenDeleteDialog(false)}>
+                <DialogTitle>Confirm Deletion</DialogTitle>
+                <DialogContent>
+                    Are you sure you want to delete "{name}"?
+                </DialogContent>
+                <DialogActions>
+                    <Button
+                        variant='contained'
+                        onClick={() => setOpenDeleteDialog(false)}
+                        disabled={loading}
+                    >
+                        Cancel
+                    </Button>
+
+                    <LoadingButton
+                        variant='contained'
+                        color='error'
+                        onClick={handleDelete}
+                        loading={loading}
+                    >
+                        Delete
+                    </LoadingButton>
+                </DialogActions>
+            </Dialog>
         </Container>
     )
 }
