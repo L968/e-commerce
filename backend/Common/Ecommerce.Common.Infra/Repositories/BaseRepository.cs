5b030a0d4426b20753466dad9e7d8e95356ac06d
Lucas Constantino <lucas.c968@gmail.com>
2024-05-07 20:14:52 -0300

    Adding support to chain properties; Adding support to enumerable properties

--- backend/Common/Ecommerce.Common.Infra/Repositories/BaseRepository.cs
+++ backend/Common/Ecommerce.Common.Infra/Repositories/BaseRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Ecommerce.Common.Infra.Exceptions;
 using Ecommerce.Common.Infra.Representation.Grid;
 using Microsoft.EntityFrameworkCore;
+using System.Collections;
 using System.Linq.Expressions;
 using System.Reflection;
 
@@ -84,21 +85,8 @@ public class BaseRepository<TContext, T>(TContext context)
     /// <returns>The filtered query.</returns>
     protected static IQueryable<T> ApplyFilter(IQueryable<T> query, FilterParams filter)
     {
-        var property = GetPropertyInfo(filter.Property);
-
-        var parameter = Expression.Parameter(typeof(T));
-        var propertyAccess = Expression.MakeMemberAccess(parameter, property);
-
-        Expression expression;
-        if (filter.Operator == "like")
-        {
-            expression = BuildLikeExpression(propertyAccess, filter.Value);
-        }
-        else
-        {
-            var value = GetConvertedValue(property.PropertyType, filter.Value);
-            expression = BuildComparisonExpression(filter.Operator, propertyAccess, value);
-        }
+        ParameterExpression parameter = Expression.Parameter(typeof(T), typeof(T).Name.ToLower());
+        Expression expression = BuildExpression(parameter, filter);
 
         var lambda = Expression.Lambda<Func<T, bool>>(expression, parameter);
         return query.Where(lambda);
@@ -136,6 +124,70 @@ public class BaseRepository<TContext, T>(TContext context)
         method = method.MakeGenericMethod(typeof(T), property.PropertyType);
 
         return (IQueryable<T>)method.Invoke(null, [query, lambda]);
+    }
+
+    /// <summary>
+    /// Builds an expression for filtering data based on the provided filter parameters.
+    /// </summary>
+    /// <param name="parameter">The parameter expression representing the input parameter of the expression.</param>
+    /// <param name="filter">The filter parameters specifying the property to filter on, the comparison operator, and the value to compare with.</param>
+    /// <returns>The expression representing the filter logic.</returns>
+    /// <exception cref="InvalidOperationException"></exception>
+    private static Expression BuildExpression(Expression parameter, FilterParams filter)
+    {
+        string[] properties = filter.Property.Split('.');
+        Expression propertyAccess = parameter;
+        Type lastPropertyType = typeof(T);
+
+        for (int i = 0; i < properties.Length; i++)
+        {
+            string property = properties[i];
+            propertyAccess = Expression.PropertyOrField(propertyAccess, property);
+            lastPropertyType = propertyAccess.Type;
+
+            if (typeof(IEnumerable).IsAssignableFrom(lastPropertyType) && lastPropertyType != typeof(string))
+            {
+                if (i == properties.Length - 1)
+                    throw new InvalidOperationException($"Last property cannot be an enumerable: {property}");
+
+                if (properties.Length > i + 2)
+                    throw new InvalidOperationException("More than one property cannot be accessed after an enumerable property");
+
+                string childProperty = properties[i + 1];
+                return BuildEnumerableExpression(lastPropertyType, childProperty, filter, propertyAccess);
+            }
+        }
+
+        object value = GetConvertedValue(lastPropertyType, filter.Value);
+        return BuildComparisonExpression((MemberExpression)propertyAccess, filter.Operator, value);
+    }
+
+    /// <summary>
+    /// Builds an expression for checking if any element in an enumerable property satisfies a condition.
+    /// </summary>
+    /// <param name="enumerableType">The type of the enumerable property.</param>
+    /// <param name="childPropertyName">The name of the property to access in each element of the enumerable.</param>
+    /// <param name="filter">The filter parameters containing the comparison operator and value.</param>
+    /// <param name="previousExpression">The previous expression representing the enumerable property.</param>
+    /// <returns>The method call expression representing the check for any element satisfying the condition.</returns>
+    private static MethodCallExpression BuildEnumerableExpression(Type enumerableType, string childPropertyName, FilterParams filter, Expression previousExpression)
+    {
+        Type enumerableGenericType = enumerableType.GetGenericArguments()[0];
+        ParameterExpression parameterExpression = Expression.Parameter(enumerableGenericType, enumerableGenericType.Name.ToLower());
+        MemberExpression propertyExpression = Expression.Property(parameterExpression, childPropertyName);
+        Type nextPropertyType = propertyExpression.Type;
+
+        object anyValue = GetConvertedValue(nextPropertyType, filter.Value);
+        var comparisonExpression = BuildComparisonExpression(propertyExpression, filter.Operator, anyValue);
+
+        var lambda = Expression.Lambda(comparisonExpression, parameterExpression);
+
+        var anyMethod = typeof(Enumerable)
+            .GetMethods()
+            .First(m => m.Name == "Any" && m.GetParameters().Length == 2)
+            .MakeGenericMethod(enumerableType.GetGenericArguments()[0]);
+
+        return Expression.Call(anyMethod, previousExpression, lambda);
     }
 
     /// <summary>
@@ -180,36 +232,25 @@ public class BaseRepository<TContext, T>(TContext context)
     }
 
     /// <summary>
-    /// Builds an expression for a "like" comparison.
-    /// </summary>
-    /// <param name="propertyAccess">The member expression representing the property access.</param>
-    /// <param name="value">The value to compare with.</param>
-    /// <returns>The method call expression representing the "like" comparison.</returns>
-    private static MethodCallExpression BuildLikeExpression(MemberExpression propertyAccess, object value)
-    {
-        var valueExpression = Expression.Constant(value);
-        return Expression.Call(propertyAccess, "Contains", null, valueExpression);
-    }
-
-    /// <summary>
     /// Builds a comparison expression based on the provided operator.
     /// </summary>
-    /// <param name="op">The comparison operator.</param>
+    /// <param name="operator">The comparison operator.</param>
     /// <param name="propertyAccess">The member expression representing the property access.</param>
     /// <param name="value">The value to compare with.</param>
     /// <returns>The binary expression representing the comparison.</returns>
-    private static BinaryExpression BuildComparisonExpression(string op, MemberExpression propertyAccess, object value)
+    private static Expression BuildComparisonExpression(MemberExpression propertyAccess, string @operator, object value)
     {
-        var constant = Expression.Constant(value);
+        ConstantExpression constant = Expression.Constant(value);
 
-        return op switch
+        return @operator switch
         {
             "=" => Expression.Equal(propertyAccess, constant),
+            "like" => Expression.Call(propertyAccess, "Contains", null, constant),
             ">" => Expression.GreaterThan(propertyAccess, constant),
-            "<" => Expression.LessThan(propertyAccess, constant),
             ">=" => Expression.GreaterThanOrEqual(propertyAccess, constant),
+            "<" => Expression.LessThan(propertyAccess, constant),
             "<=" => Expression.LessThanOrEqual(propertyAccess, constant),
-            _ => throw new ArgumentException($"Invalid operator: {op}"),
+            _ => throw new ArgumentException($"Invalid operator: {@operator}"),
         };
     }
 }
