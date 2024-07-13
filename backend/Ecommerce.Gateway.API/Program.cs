084229932e94cb246bdcb88c00b9969d20e859f8
Lucas Constantino <lucas.c968@gmail.com>
2024-07-13 14:35:44 -0300

    Adding first authorization requirement to the gateway

--- backend/Ecommerce.Gateway.API/Program.cs
+++ backend/Ecommerce.Gateway.API/Program.cs
@@ -1,11 +1,29 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+builder.Services.AddAuthentication(auth =>
+{
+    auth.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    auth.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+});
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("customPolicy", policy =>
+        policy.RequireAuthenticatedUser());
+});
+
 var app = builder.Build();
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
 
 app.MapReverseProxy();
 
